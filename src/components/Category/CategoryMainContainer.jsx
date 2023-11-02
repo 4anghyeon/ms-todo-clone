7d05cf07bff8e7ac33013edb5328a16185abe7ae
sanghyeon <uphyeon@kakao.com>
2023-11-02 18:48:15 +0900

    목록 이름 변경 완료

--- src/components/Category/CategoryMainContainer.jsx
+++ src/components/Category/CategoryMainContainer.jsx
@@ -3,15 +3,23 @@ import style from './CategoryContainer.module.css';
 import CategoryButtonContainer from './CategoryButtonContainer';
 import CategoryListContainer from './CategoryListContainer';
 
+class Category {
+  constructor(name, isGroup) {
+    this.group = isGroup;
+    this.name = name;
+    this.isEdit = false;
+  }
+}
+
 const CategoryContainer = () => {
-  const [categoryList, setCategoryList] = useState([{group: null, name: '알고리즘'}]);
+  const [categoryList, setCategoryList] = useState([new Category('알고리즘')]);
 
   return (
     <div className={style.container}>
       {/* 1. 검색 */}
       <div>검색</div>
       {/* 2. 목록  */}
-      <CategoryListContainer categoryList={categoryList} />
+      <CategoryListContainer categoryList={categoryList} setCategoryList={setCategoryList} />
       {/* 3. 추가 버튼 */}
       <CategoryButtonContainer setCategoryList={setCategoryList} />
     </div>
